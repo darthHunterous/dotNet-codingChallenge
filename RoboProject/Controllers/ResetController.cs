164bcb9685d725b66931570f7e5f1523ce4cd496
Édio Cerati Neto <edioceratineto@gmail.com>
2022-11-07 03:00:55 -0300

    Add Reset Controller at startup

---
+++ RoboProject/Controllers/ResetController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoboProject.Entities;
+using RoboProject.Entities.Interfaces;
+
+namespace RoboProject.Controllers
+{
+    [ApiController]
+    [Route("/api/robo/reset")]
+    public class ResetController : ControllerBase
+    {
+        private readonly ILogger<RoboController> _logger;
+        private IDeserializerHelper _deserializerHelper;
+
+        public ResetController(ILogger<RoboController> logger, IDeserializerHelper deserializerHelper)
+        {
+            _logger = logger;
+            _deserializerHelper = deserializerHelper;
+        }
+
+        [HttpGet(Name = "GetRoboReset")]
+        public IActionResult Get()
+        {
+            Robo robo = new();
+
+            _deserializerHelper.SerializeRobo(robo);
+
+            return Redirect("https://localhost:7186/swagger/index.html");
+        }
+    }
+}
