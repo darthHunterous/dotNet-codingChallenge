307f61bfff5a22ed4ce21cd9f3bc678cb9999f9f
Édio Cerati Neto <edioceratineto@gmail.com>
2022-11-07 02:13:42 -0300

    Finish StaticArmState unit test

---
+++ RoboProjectUnitTests/SlightlyContractedArmState.test.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using RoboProject.Entities;
+using RoboProject.Entities.ArmStates;
+
+namespace RoboProjectUnitTests
+{
+    [TestFixture]
+    public class SlightlyContractedArmStateTest
+    {
+        private SlightlyContractedArmState _instance;
+
+        [SetUp]
+        public void Setup()
+        {
+            _instance = new SlightlyContractedArmState();
+        }
+
+        [Test]
+        public void HeavilyContractedArmState_ContractArm_ShouldInvokeArmChangeStateToContractedArmState()
+        {
+            // Arrange
+            Arm arm = new();
+            arm.State = new SlightlyContractedArmState();
+
+            // Act
+            _instance.ContractArm(arm);
+
+            // Assert
+            Assert.That(arm.State, Is.InstanceOf<ContractedArmState>());
+        }
+
+        [Test]
+        public void HeavilyContractedArmState_RelaxArm_ShouldInvokeArmChangeStateToStaticArmState()
+        {
+            // Arrange
+            Arm arm = new();
+            arm.State = new SlightlyContractedArmState();
+
+            // Act
+            _instance.RelaxArm(arm);
+
+            // Assert
+            Assert.That(arm.State, Is.InstanceOf<StaticArmState>());
+        }
+
+        [Test]
+        public void HeavilyContractedArmState_RotateWristClockwise_ShouldThrowAnException()
+        {
+            // Arrange
+            Arm arm = new();
+            arm.State = new SlightlyContractedArmState();
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => _instance.RotateWristClockwise(arm));
+        }
+
+        [Test]
+        public void HeavilyContractedArmState_RotateWristCounterClockwise_ShouldThrowAnException()
+        {
+            // Arrange
+            Arm arm = new();
+            arm.State = new SlightlyContractedArmState();
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => _instance.RotateWristCounterClockwise(arm));
+        }
+    }
+}
---
+++ RoboProjectUnitTests/StaticArmState.test.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using RoboProject.Entities;
+using RoboProject.Entities.ArmStates;
+
+namespace RoboProjectUnitTests
+{
+    [TestFixture]
+    public class StaticArmStateTest
+    {
+        private StaticArmState _instance;
+
+        [SetUp]
+        public void Setup()
+        {
+            _instance = new StaticArmState();
+        }
+
+        [Test]
+        public void HeavilyContractedArmState_ContractArm_ShouldInvokeArmChangeStateToSlightlyContractedArmState()
+        {
+            // Arrange
+            Arm arm = new();
+            arm.State = new StaticArmState();
+
+            // Act
+            _instance.ContractArm(arm);
+
+            // Assert
+            Assert.That(arm.State, Is.InstanceOf<SlightlyContractedArmState>());
+        }
+
+        [Test]
+        public void HeavilyContractedArmState_RelaxArm_ShouldThrowAnException()
+        {
+            // Arrange
+            Arm arm = new();
+            arm.State = new StaticArmState();
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => _instance.RelaxArm(arm));
+        }
+
+        [Test]
+        public void HeavilyContractedArmState_RotateWristClockwise_ShouldThrowAnException()
+        {
+            // Arrange
+            Arm arm = new();
+            arm.State = new StaticArmState();
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => _instance.RotateWristClockwise(arm));
+        }
+
+        [Test]
+        public void HeavilyContractedArmState_RotateWristCounterClockwise_ShouldThrowAnException()
+        {
+            // Arrange
+            Arm arm = new();
+            arm.State = new StaticArmState();
+
+            // Act & Assert
+            Assert.Throws<Exception>(() => _instance.RotateWristCounterClockwise(arm));
+        }
+    }
+}
